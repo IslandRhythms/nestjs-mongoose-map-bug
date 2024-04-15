761e9776dc11c6ef65906cc9f4bc785990a61d58
Valeri Karpov <val@karpov.io>
2024-04-15 10:02:01 -0400

    working fix for Automattic/mongoose#14454

--- schema.ts
+++ schema.ts
@@ -3,13 +3,13 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 @Schema()
 export class OAuth2 {
   @Prop()
-  url: string;
+  url?: string;
 
   @Prop({ required: true })
-  clientId: string;
+  clientId?: string;
 
   @Prop({ required: true })
-  clientSecret: string;
+  clientSecret?: string;
 
   @Prop()
   scope?: string;
@@ -18,14 +18,21 @@ export class OAuth2 {
 export const OAuth2Schema = SchemaFactory.createForClass(OAuth2);
 
 @Schema()
+export class Endpoint {
+  @Prop({ type: 'ObjectId', ref: 'OAuth2' })
+  OAuth?: string;
+}
+export const EndpointSchema = SchemaFactory.createForClass(Endpoint);
+
+@Schema()
 export class Connector {
   @Prop({ required: true, unique: true })
-  tenantId: string;
+  tenantId?: string;
 
   @Prop({ type: 'ObjectId', ref: 'OAuth2' })
   serviceAccount?: string;
 
-  @Prop({ type: Map, of: 'Mixed' })
+  @Prop({ type: Map, of: EndpointSchema })
   endpoints?: Map<string, any>;
 }
 
